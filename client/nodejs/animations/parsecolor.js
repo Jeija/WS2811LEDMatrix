0c1cdec4b7aa0ee5ebf2a77805c627c806438e9b
Jeija <norrepli@gmail.com>
2015-01-24 18:34:05 +0100

    Add transitions to pixel art animation

--- client/nodejs/animations/parsecolor.js
+++ client/nodejs/animations/parsecolor.js
@@ -2,9 +2,9 @@ module.exports = function (colorstring) {
 	var color;
 	switch (colorstring) {
 		case "white":	return { red : 255, green : 255, blue : 255 };
-		case "red":	return { red : 255 };
-		case "green":	return { green : 255 };
-		case "blue":	return { blue : 255 };
-		default:	return { red : 0, green : 0, blue : 0 };
+		case "red":	return { red : 255, green :   0, blue : 0   };
+		case "green":	return { red :   0, green : 255, blue : 0   };
+		case "blue":	return { red :   0, green :   0, blue : 255 };
+		default:	return { red :   0, green :   0, blue :   0 };
 	}
 };
--- client/nodejs/animations/pixelart.js
+++ client/nodejs/animations/pixelart.js
@@ -1,3 +1,4 @@
+var parseColor = require("./parsecolor");
 var getPixels = require("get-pixels");
 var path = require("path");
 var fs = require("fs");
@@ -6,10 +7,22 @@ var IMAGES_DIR = "pixelart";
 var matrices = ["left", "right", "topLeft", "topRight"];
 var images = [];
 var currentImg = {};
-var change;
+var change, decay, transition, transitionColor, interval, beat, decay;
 
 function init (matrix, settings) {
+	// Parse settings
 	change = settings.change;
+	transition = settings.transition;
+	transitionColor = settings.transitionColor;
+	switch (settings.transitionDecay) {
+		case "ultrafast":	decay = 0.1;	break;
+		case "fast":		decay = 0.05;	break;
+		case "normal":		decay = 0.03;	break;
+		case "slow":		decay = 0.015;	break;
+		default:		decay = 0.03;
+	}
+
+	// Read image files from pixelart directory
 	var imgfiles = fs.readdirSync(path.join(__dirname, IMAGES_DIR));
 	for (var i in imgfiles) {
 		console.log("Loading " + imgfiles[i]);
@@ -18,19 +31,30 @@ function init (matrix, settings) {
 			else images.push(pixels);
 		});
 	}
+
+	interval = setInterval(function () {
+		if (beat > decay) beat -= decay;
+		else beat = 0;
+	}, 10);
 }
 
 function draw (matrix) {
 	matrix.clear();
+	var trCol = transition == "glow" ? parseColor(transitionColor) :
+		{ red : 0, green : 0, blue : 0 };
+	trCol.red *= beat;
+	trCol.green *= beat;
+	trCol.blue *= beat;
+
 	for (var mtx in matrices) {
 		var img = currentImg[matrices[mtx]];
 		if (img === undefined || !images[img]) continue;
 		for (var x = 0; x < 10; x++) {
 			for (var y = 0; y < 10; y++) {
 				var col = {
-					red : images[img].get(x, y, 0),
-					green : images[img].get(x, y, 1),
-					blue : images[img].get(x, y, 2)
+					red : Math.min(255, images[img].get(x, y, 0) + trCol.red),
+					green : Math.min(255, images[img].get(x, y, 1) + trCol.green),
+					blue : Math.min(255, images[img].get(x, y, 2) + trCol.blue)
 				};
 				matrix.setPixelMatrix(matrices[mtx], x, y, col);
 			}
@@ -48,19 +72,21 @@ function newImgId(oldid) {
 function event (ev) {
 	// Assign random new image to every matrix on beat
 	if (ev == "beat") {
+		beat = 1;
 		if (change == "everything") {
 			for (var i in matrices) {
 				currentImg[matrices[i]] = newImgId(currentImg[matrices[i]]);
 			}
 		} else if (change == "one") {
-			var i = Math.round(Math.random() * 4);
-			currentImg[matrices[i]] = newImgId(currentImg[matrices[i]]);
+			var j = Math.round(Math.random() * 4);
+			currentImg[matrices[j]] = newImgId(currentImg[matrices[j]]);
 		}
 	}
 }
 
 function terminate () {
 	images = [];
+	clearInterval(interval);
 }
 
 module.exports = {
@@ -68,7 +94,10 @@ module.exports = {
 		name : "Pixel Art",
 		init : init,
 		settings : {
-			change : [ "everything", "one" ]
+			change : [ "everything", "one" ],
+			transition : [ "none", "glow" ],
+			transitionColor : [ "white", "red", "green", "blue" ],
+			transitionDecay : [ "fast", "ultrafast", "normal", "slow" ]
 		},
 		draw : draw,
 		event : event,
