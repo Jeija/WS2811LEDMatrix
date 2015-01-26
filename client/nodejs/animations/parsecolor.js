0ba50aa7e2ba7a741af2868006fe47632781af47
Jeija <norrepli@gmail.com>
2015-01-26 17:27:48 +0100

    Parse colors with npm "parse-color" module, this makes parsing colors a lot more flexible: e.g. it also accepts hex color codes

--- client/nodejs/animations/parsecolor.js
+++ client/nodejs/animations/parsecolor.js
@@ -1,11 +1,10 @@
+var parse_color = require("parse-color");
+
 module.exports = function (colorstring) {
-	var color;
-	switch (colorstring) {
-		case "white":	return { red : 255, green : 255, blue : 255 };
-		case "red":	return { red : 255, green :   0, blue : 0   };
-		case "green":	return { red :   0, green : 255, blue : 0   };
-		case "blue":	return { red :   0, green :   0, blue : 255 };
-		case "black":	return { red :   0, green :   0, blue :   0 };
-		default:	return { red :   0, green :   0, blue :   0 };
-	}
+	var color = parse_color(colorstring);
+	return {
+		red : color.rgb[0],
+		green : color.rgb[1],
+		blue : color.rgb[2]
+	};
 };
