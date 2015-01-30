6d4fffe629d3b5c41b8db4b0e876b97606db9e51
Jeija <norrepli@gmail.com>
2015-01-30 21:46:26 +0100

    Bugfixes and simple gamma correction for video animation

--- client/nodejs/animations/video.js
+++ client/nodejs/animations/video.js
@@ -9,12 +9,10 @@ var XOFFSET = 10;
 var YOFFSET = 10;
 
 var ready = false;
-var url, frames, time, fps, starttime;
+var url, frames, fps, starttime;
 
 function init (matrix, settings) {
-	starttime = Date.now();
 	frames = [];
-	time = 0;
 	url = settings.url;
 	fps = parseFloat(settings.fps);
 	ready = false;
@@ -35,16 +33,18 @@ function init (matrix, settings) {
 
 	var i = 0;
 	setInterval(function () {
-		open_files++;
-
 		for (var j = 0; j <= MAX_OPEN_FILES; j++) {
+			open_files++;
 			if (open_files > MAX_OPEN_FILES) return;
 			if (!imgfiles[i]) return;
 			getPixels(path.join(url, imgfiles[i++]), function (err, pixels) {
 				open_files--;
 				if (err) console.log(err);
 				else frames.push(pixels);
-				if (i == imgfiles.length) ready = true;
+				if (i == imgfiles.length) {
+					starttime = Date.now();
+					ready = true;
+				}
 			});
 		}
 	}, 1);
@@ -54,7 +54,6 @@ function draw (matrix) {
 	if (!ready) return;
 
 	var time_seconds = (Date.now() - starttime) / 1000;
-	console.log(time_seconds);
 	var framenum = Math.round(time_seconds * fps);
 	if (!frames[framenum]) return;
 	var frame = frames[framenum];
@@ -66,6 +65,12 @@ function draw (matrix) {
 				green : frame.get(x, y, 1),
 				blue : frame.get(x, y, 2)
 			}
+
+			// Simple gamma correction:
+			rgb.red = 255 * Math.pow(rgb.red/255, 1/0.45);
+			rgb.green = 255 * Math.pow(rgb.green/255, 1/0.45);
+			rgb.blue = 255 * Math.pow(rgb.blue/255, 1/0.45);
+
 			matrix.setPixelGlobal(x + XOFFSET, y + YOFFSET, rgb);
 		}
 	}
@@ -82,13 +87,13 @@ module.exports = {
 		name : "Video",
 		settings : {
 			url : "Path to frames directory",
-			fps : "23.976"
+			fps : "30"
 		},
 		event : event,
 		init : init,
 		draw : draw,
 		terminate : terminate,
 		description : "Display video on matrix, must be downscaled to 20x10 pixels png images, \
-				e.g. ffmpeg -i <input> -vf scale=20:10 -strict -2 <outdir>/%05d.png"
+				e.g. ffmpeg -i <input> -vf scale=20:10 <outdir>/%05d.png"
 	}
 };
